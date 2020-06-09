3137fe361616e9238be96b8d75dd244c0546c3f2
Alice347 <66677798+Alice347@users.noreply.github.com>
2020-06-09 18:57:24 +0200

    Comentario añadido al código

--- Proyecto/src/paquete/HolaMundo.java
+++ Proyecto/src/paquete/HolaMundo.java
@@ -1,7 +1,7 @@
 package paquete;
 
 public class HolaMundo {
-
+//Modificar el código
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hola Mundo  ");
